2e07159b4e4d057e82bc80614a726ef3440deed7
Sumit Paswan <sumitpaswan942@gmail.com>
2022-08-28 18:52:58 +0530

    login success redirect

--- src/main/java/com/example/textile/security/SecurityConfiguration.java
+++ src/main/java/com/example/textile/security/SecurityConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
 @Configuration
@@ -21,6 +23,12 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+         http.formLogin().defaultSuccessUrl("/invoices");
+         return http.build();
     }
 
     @Bean
