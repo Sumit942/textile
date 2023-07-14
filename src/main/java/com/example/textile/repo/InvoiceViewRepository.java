aee6b3794e1409fffc86ad802618b40fde6b13be
Sumit Paswan <sumitpaswan942@gmail.com>
2023-07-14 22:52:32 +0530

    added report by ChNo sumittest

--- src/main/java/com/example/textile/repo/InvoiceViewRepository.java
+++ src/main/java/com/example/textile/repo/InvoiceViewRepository.java
@@ -3,7 +3,6 @@ package com.example.textile.repo;
 import com.example.textile.entity.InvoiceView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
 
@@ -13,7 +12,6 @@ public interface InvoiceViewRepository extends JpaRepository<InvoiceView,Long> {
     @Query("FROM InvoiceView view ORDER BY view.invoiceNo DESC")
     List<InvoiceView> findAll();
 
-    @Async
     @Query("FROM InvoiceView view WHERE view.invoiceId IN (:invoiceIds) ORDER BY view.invoiceNo DESC")
     List<InvoiceView> findByInvoiceId(List<Long> invoiceIds);
 }
