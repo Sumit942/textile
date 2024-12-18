7423613609797c962c7c916ad9a1ffbea4b14769
Sumit <sumitpaswan942@gmail.com>
2024-12-19 03:55:50 +0530

    React index.html added in resources

--- src/main/java/com/example/textile/controller/ReactController.java
+++ src/main/java/com/example/textile/controller/ReactController.java
@@ -1,13 +1,16 @@
 package com.example.textile.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class ReactController {
 
     @RequestMapping("/react/**")
     public String redirectToReact() {
+        log.info("redirectToReact() Entry");
         // Forward to React's index.html located in the static/react folder
         return "forward:/static/react/index.html";
     }
